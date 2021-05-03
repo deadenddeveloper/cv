f5410e074fb9ff52a608809336ccdf02addd18a1
Roman Bobrik <romanbobrik@tut.by>
2021-05-03 23:14:08 +0200

    added left side (photo, name, post, social, cv link)

---
+++ components/markup/CvLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+    <a
+      class="inline-block h-10 px-5 py-1.5 text-lg text-skin-inverted rounded-full bg-skin-accent hover:bg-skin-accent-hover focus:outline-none focus:ring-2"
+      download="download"
+      target="_blank"
+      :href="`cv/romanbobrik_${currentLocale.code}.pdf`"
+    >
+      <fa-icon icon="file-pdf" />
+      <span>{{ $t('cv.download') }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    currentLocale() {
+      return this.$i18n.locales.filter(i => i.code === this.$i18n.locale)[0];
+    },
+  }
+}
+</script>
---
+++ components/markup/SocialLinks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex justify-around">
+    <a
+      v-for="link in links"
+      :key="link.name"
+      :title="link.name"
+      :href="link.url"
+      target="_blank"
+      class="w-10 h-10 flex items-center justify-center rounded-full text-2xl transition-colors duration-200 cursor-pointer focus:outline-none focus:ring-2 text-skin-accent hover:text-skin-accent-hover"
+    >
+      <fa-icon :icon="['fab', link.icon]" />
+    </a>
+  </div>
+</template>
+
+<script>
+import links from '~/data/links'
+
+export default {
+  data: () => ({
+    links,
+  }),
+}
+</script>
