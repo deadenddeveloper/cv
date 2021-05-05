1a120ec5651b461d0ad6376e75eee79913334109
Roman Bobrik <romanbobrik@tut.by>
2021-05-05 23:24:19 +0200

    section header abstracted into component

---
+++ components/ui/SectionHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3 class="text-center lg:text-left text-4xl text-skin-accent">
+    <fa-icon :icon="icon" />
+    <span>{{ title }}</span>
+  </h3>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    icon: String,
+  },
+}
+</script>
