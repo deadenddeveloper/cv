e9eb692624f7352791976513f60393b677567554
romanbobrik <romanbobrik@tut.by>
2021-12-07 19:42:17 +0100

    pdf CVs renewed

--- components/markup/CvLink.vue
+++ components/markup/CvLink.vue
@@ -2,9 +2,9 @@
   <div class="text-center">
     <a
       class="inline-block h-10 px-5 py-1.5 text-lg text-skin-inverted rounded-full bg-skin-accent hover:bg-skin-accent-hover focus:outline-none focus:ring-2"
-      :download="`romanbobrik_${currentLocale.code}.pdf`"
+      :download="`romanmeyer_${currentLocale.code}.pdf`"
       target="_blank"
-      :href="`cv/romanbobrik_${currentLocale.code}.pdf`"
+      :href="`cv/romanmeyer_${currentLocale.code}.pdf`"
     >
       <fa-icon icon="file-pdf" />
       <span>{{ $t('cv.download') }}</span>
