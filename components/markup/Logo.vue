bad70c15b73b2b27d958dbae413344d91e60474f
Roman Bobrik <romanbobrik@tut.by>
2022-10-22 20:31:34 +0200

    added fsevents as optional dependency to avoid error by deployment

---
+++ components/markup/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <img
+  class="h-12 lg:h-auto"
+  width="173"
+  height="47"
+  loading="lazy"
+  alt="Logo"
+  :src="require(`~/assets/img/logo.webp`)"
+  />
+</template>
---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col space-y-8 h-screen items-center justify-center">
+    <markup-logo />
+    <h1 class="text-skin-accent text-4xl">404</h1>
+    <h2 class="text-2xl">{{ $t('page_not_found') }}</h2>
+    <nuxt-link
+      class="inline-block h-10 px-5 py-1.5 text-lg text-skin-inverted rounded-full bg-skin-accent hover:bg-skin-accent-hover focus:outline-none focus:ring-2" to="/">{{ $t('go_home') }}</nuxt-link>
+  </div>
+</template>
